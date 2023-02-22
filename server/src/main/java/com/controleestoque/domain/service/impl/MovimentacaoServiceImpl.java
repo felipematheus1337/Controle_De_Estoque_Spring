b09a03bc6857686e5be988b8f814b9c4168e96f1
felipematheus1337 <lipehbr@gmail.com>
2023-02-22 20:18:59 -0300

    Finalizado implementação do CRUD das 3 entidades!

--- server/src/main/java/com/controleestoque/domain/service/impl/MovimentacaoServiceImpl.java
+++ server/src/main/java/com/controleestoque/domain/service/impl/MovimentacaoServiceImpl.java
@@ -1,36 +1,61 @@
 package com.controleestoque.domain.service.impl;
 
 import com.controleestoque.api.dto.MovimentacaoDTO;
+import com.controleestoque.api.mapper.MovimentacaoMapper;
+import com.controleestoque.domain.repository.MovimentacaoRepository;
 import com.controleestoque.domain.service.MovimentacaoService;
+import com.controleestoque.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MovimentacaoServiceImpl  implements MovimentacaoService {
 
+    private final MovimentacaoRepository repository;
+    private final MovimentacaoMapper mapper;
+
     @Override
-    public MovimentacaoDTO create(MovimentacaoDTO MovimentacaoDTO) {
-        return null;
+    public MovimentacaoDTO create(MovimentacaoDTO movimentacaoDTO) {
+        var moviment = mapper.toEntity(movimentacaoDTO);
+        return mapper.toDTO(repository.save(moviment));
     }
 
     @Override
     public MovimentacaoDTO getById(Long id) {
-        return null;
+        var movimentExists = repository.findById(id);
+
+        if(movimentExists.isEmpty()) {
+            throw new BusinessException("moviment don't exist with that ID!");
+        }
+        return mapper.toDTO(movimentExists.get());
     }
 
     @Override
     public List<MovimentacaoDTO> findAll() {
-        return null;
+        return mapper.toListOfDTO(repository.findAll());
     }
 
     @Override
     public MovimentacaoDTO edit(Long id,MovimentacaoDTO dto) {
-        return null;
+        var movimentExists = repository.findById(id);
+
+        if(movimentExists.isEmpty()) {
+            throw new BusinessException("moviment don't exist with that ID!");
+        }
+        dto.setId(id);
+        return mapper.toDTO(repository.save(movimentExists.get()));
     }
 
     @Override
     public void delete(Long id) {
+        var movimentExists = repository.findById(id);
 
+        if(movimentExists.isEmpty()) {
+            throw new BusinessException("moviment don't exist with that ID!");
+        }
+        repository.deleteById(id);
     }
 }
--- server/src/main/java/com/controleestoque/domain/service/impl/ProdutoServiceImpl.java
+++ server/src/main/java/com/controleestoque/domain/service/impl/ProdutoServiceImpl.java
@@ -1,36 +1,61 @@
 package com.controleestoque.domain.service.impl;
 
 import com.controleestoque.api.dto.ProdutoDTO;
+import com.controleestoque.api.mapper.ProdutoMapper;
+import com.controleestoque.domain.repository.ProdutoRepository;
 import com.controleestoque.domain.service.ProdutoService;
+import com.controleestoque.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProdutoServiceImpl implements ProdutoService {
 
+    private final ProdutoRepository repository;
+    private final ProdutoMapper mapper;
+
     @Override
-    public ProdutoDTO create(ProdutoDTO ProdutoDTO) {
-        return null;
+    public ProdutoDTO create(ProdutoDTO produtoDTO) {
+        var product = mapper.toEntity(produtoDTO);
+        return mapper.toDTO(repository.save(product));
     }
 
     @Override
     public ProdutoDTO getById(Long id) {
-        return null;
+        var productExists = repository.findById(id);
+
+        if(productExists.isEmpty()) {
+            throw new BusinessException("Product don't exist with that ID!");
+        }
+        return mapper.toDTO(productExists.get());
     }
 
     @Override
     public List<ProdutoDTO> findAll() {
-        return null;
+        return mapper.toDTOList(repository.findAll());
     }
 
     @Override
     public ProdutoDTO edit(Long id,ProdutoDTO dto) {
-        return null;
+        var productExists = repository.findById(id);
+
+        if(productExists.isEmpty()) {
+            throw new BusinessException("product don't exist with that ID!");
+        }
+        dto.setId(id);
+        return mapper.toDTO(repository.save(productExists.get()));
     }
 
     @Override
     public void delete(Long id) {
+        var productExists = repository.findById(id);
 
+        if(productExists.isEmpty()) {
+            throw new BusinessException("product don't exist with that ID!");
+        }
+        repository.deleteById(id);
     }
 }
